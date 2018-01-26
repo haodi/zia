a978a07f3250477119be4bb74b6bc7945bab1c30
lihaodi <haodilee@foxmail.com>
2018-01-26 21:44:04 +0800

    renamed

--- zia-eureka/src/main/java/com/yy/zia/eureka/eurekaApplication.java
+++ zia-eureka/src/main/java/com/yy/zia/eureka/eurekaApplication.java
@@ -4,15 +4,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-/**
- * Created by lihaodi on 2017/5/30.
- * 服务注册中心
- */
-
 @EnableEurekaServer
 @SpringBootApplication
-public class eurekaServer {
+public class eurekaApplication {
     public static void main(String[] args) {
-        new SpringApplicationBuilder(eurekaServer.class).web(true).run(args);
+        new SpringApplicationBuilder(eurekaApplication.class).web(false).run(args);
     }
 }
