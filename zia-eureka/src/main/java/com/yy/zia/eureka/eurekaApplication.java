34fabca63a8a7f122d48cf520a5835078b37511f
lihaodi <haodilee@foxmail.com>
2018-01-27 10:23:08 +0800

    格式化代码

--- zia-eureka/src/main/java/com/yy/zia/eureka/eurekaApplication.java
+++ zia-eureka/src/main/java/com/yy/zia/eureka/eurekaApplication.java
@@ -1,13 +1,13 @@
 package com.yy.zia.eureka;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
 public class eurekaApplication {
     public static void main(String[] args) {
-        new SpringApplicationBuilder(eurekaApplication.class).web(false).run(args);
+        SpringApplication.run(eurekaApplication.class, args);
     }
 }
