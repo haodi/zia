311103923380ac99ae0641595fdb347f7d902d71
lihaodi <haodilee@foxmail.com>
2018-03-18 15:52:42 +0800

    Adding DefaultFallbackProvider

---
+++ zia-zuul/src/main/java/com/learning/zuul/configuration/DefaultFallbackProvider.java
@@ -0,0 +1,81 @@
+package com.learning.zuul.configuration;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.netflix.hystrix.exception.HystrixTimeoutException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class DefaultFallbackProvider implements FallbackProvider {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Override
+    public String getRoute() {
+        return "*";
+    }
+
+    @Override
+    public ClientHttpResponse fallbackResponse(final Throwable cause) {
+        if (cause instanceof HystrixTimeoutException) {
+            return response(HttpStatus.GATEWAY_TIMEOUT);
+        } else {
+            return response(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Override
+    public ClientHttpResponse fallbackResponse() {
+        return response(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private ClientHttpResponse response(final HttpStatus status) {
+        return new ClientHttpResponse() {
+            @Override
+            public HttpStatus getStatusCode() {
+                return status;
+            }
+
+            @Override
+            public int getRawStatusCode() {
+                return status.value();
+            }
+
+            @Override
+            public String getStatusText() {
+                return status.getReasonPhrase();
+            }
+
+            @Override
+            public void close() {
+            }
+
+            @Override
+            public InputStream getBody() throws JsonProcessingException {
+                Map<String, String> result = new HashMap<>();
+                result.put("message", "Service is down!");
+
+                return new ByteArrayInputStream(objectMapper.writeValueAsBytes(result));
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_JSON);
+                return headers;
+            }
+        };
+    }
+}
